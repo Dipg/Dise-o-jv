556db18a7c7b57dcb997e287eb6d51f243860d27
Diego <isaacdiego25@gmail.com>
2021-09-07 15:10:36 -0500

    Se logro separar por ahraea ala hora de mstrar

--- script.js
+++ script.js
@@ -30,8 +30,13 @@ function UpdateCost()  {
 
 
     document.getElementById('Bloque').innerHTML = "";
+    document.getElementById('Bloque2').innerHTML = "";
+  
+ 
 
-    
+
+   
+
     for(const number of getSelectedCheckboxValues('color') ){
     var Bloque= document.getElementById('Bloque');
     const todo= document.createElement("div");
@@ -63,4 +68,51 @@ function UpdateCost()  {
     Cuarto.appendChild(Quinto);
  
     }
+
+
+
+
+
+
+
+    
+    for(const number of getSelectedCheckboxValues('colores') ){
+        var Bloque= document.getElementById('Bloque2');
+        const todo= document.createElement("div");
+    
+        todo.classList.add('page__toggle');
+        Bloque2.appendChild(todo);
+    
+        const Segundo= document.createElement("label");
+    
+        Segundo.classList.add('toggle');
+        todo.appendChild(Segundo);
+    
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = true;
+        checkbox.disabled = true;
+        checkbox.classList.add('toggle__input', 'form-check-input');
+        Segundo.appendChild(checkbox );
+    
+        var Cuarto= document.createElement('span');
+    
+     
+        Cuarto.classList.add('toggle__label');
+        Segundo.appendChild(Cuarto );
+    
+        var Quinto= document.createElement('span');
+        Quinto.textContent = number;
+        Quinto.classList.add('toggle__text');
+        Cuarto.appendChild(Quinto);
+     
+        }
+
+
+
+
+
+
+
+
 }
